4fa727c1cd0f2679212c121224ddc1898b3bc3a2
iraianbu <iraianbu_govind@thbs.com>
2023-12-08 12:56:39 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -1,29 +1,8 @@
 public class Hello{
     public static void main(String[] args) {
-        String original = "Hello, World!";
-        String reversed = reverseString(original);
-
-        System.out.println("Original String: " + original);
-        System.out.println("Reversed String: " + reversed);
+        
+        System.out.println("hello this is anbu" );
     }
 
-    public static String reverseString(String str) {
-        char[] charArray = str.toCharArray();
-        int start = 0;
-        int end = charArray.length - 1;
 
-        while (start < end) {
-            // Swap characters at start and end indices
-            char temp = charArray[start];
-            charArray[start] = charArray[end];
-            charArray[end] = temp;
-
-            // Move towards the center
-            start++;
-            end--;
-        }
-
-        // Convert the character array back to a string
-        return new String(charArray);
-    }
 }
